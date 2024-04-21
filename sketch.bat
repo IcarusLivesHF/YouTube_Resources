a27046d53c17dd28647d58d7ebddc796de77a249
Icarus Lives <fg92sk8r@yahoo.com>
2024-04-21 08:38:48 -0400

    sketch.bat
    Basic template to follow along with YouTube videos.

--- sketch.bat
+++ sketch.bat
@@ -1,6 +1,8 @@
 @echo off & setlocal enableDelayedExpansion & call :init
 
 rem Font: MxPlus IBM EGA - 5x5 (https://discord.gg/GXYTgX9eYz - thread.ID: 1226690191620575252)
+
+rem YOUR CODE HERE --------v
 set /a "hei=wid=100" & mode !wid!,!hei!
 
 
@@ -9,7 +11,7 @@ set /a "hei=wid=100" & mode !wid!,!hei!
 
 
 
-
+rem -----------------------^
 
 :init
 (set \n=^^^
