fede930e872f4d10ebd86e33d060deeb5fe9d7c1
Icarus Lives <fg92sk8r@yahoo.com>
2024-04-30 19:31:48 -0400

    forward_and_steering.bat

---
+++ forward_and_steering.bat
@@ -0,0 +1,47 @@
+@echo off & setlocal enableDelayedExpansion & call :init
+
+rem Font: MxPlus IBM EGA - 5x5 (https://discord.gg/GXYTgX9eYz - thread.ID: 1226690191620575252)
+set /a "hei=wid=100" & mode !wid!,!hei!
+
+set "dot=%\e%[A%\e%[48;5;15m %\e%[B%\e%[2D %\e%[C %\e%[B%\e%[2D %\e%[0m"
+
+
+pause >nul
+exit
+
+:init
+(set \n=^^^
+%= This creates an escaped Line Feed - DO NOT ALTER =%
+)
+(for /f %%a in ('echo prompt $E^| cmd') do set "\e=%%a" )
+<nul set /p "=%\e%[?25l"
+
+set "sin=(a=((x*31416/180)%%62832)+(((x*31416/180)%%62832)>>31&62832), b=(a-15708^a-47124)>>31,a=(-a&b)+(a&~b)+(31416&b)+(-62832&(47123-a>>31)),a-a*a/1875*a/320000+a*a/1875*a/15625*a/16000*a/2560000-a*a/1875*a/15360*a/15625*a/15625*a/16000*a/44800000) / 10000"
+set "cos=(a=((15708-x*31416/180)%%62832)+(((15708-x*31416/180)%%62832)>>31&62832), b=(a-15708^a-47124)>>31,a=(-a&b)+(a&~b)+(31416&b)+(-62832&(47123-a>>31)),a-a*a/1875*a/320000+a*a/1875*a/15625*a/16000*a/2560000-a*a/1875*a/15360*a/15625*a/15625*a/16000*a/44800000) / 10000"
+
+rem line x0 y0 x1 y1 color
+set line=for %%# in (1 2) do if %%#==2 ( for /f "tokens=1-5" %%1 in ("^!args^!") do (%\n%
+	if "%%~5" equ "" ( set "hue=15" ) else ( set "hue=%%~5")%\n%
+	set "$line=[48;5;^!hue^!m"%\n%
+	set /a "xa=%%~1", "ya=%%~2", "xb=%%~3", "yb=%%~4", "dx=%%~3 - %%~1", "dy=%%~4 - %%~2"%\n%
+	if ^^!dy^^! lss 0 ( set /a "dy=-dy", "stepy=-1" ) else ( set "stepy=1" )%\n%
+	if ^^!dx^^! lss 0 ( set /a "dx=-dx", "stepx=-1" ) else ( set "stepx=1" )%\n%
+	set /a "dx<<=1", "dy<<=1"%\n%
+	if ^^!dx^^! gtr ^^!dy^^! (%\n%
+		set /a "fraction=dy - (dx >> 1)"%\n%
+		for /l %%x in (^^!xa^^!,^^!stepx^^!,^^!xb^^!) do (%\n%
+			if ^^!fraction^^! geq 0 set /a "ya+=stepy", "fraction-=dx"%\n%
+			set /a "fraction+=dy"%\n%
+			set "$line=^!$line^![^!ya^!;%%xH "%\n%
+		)%\n%
+	) else (%\n%
+		set /a "fraction=dx - (dy >> 1)"%\n%
+		for /l %%y in (^^!ya^^!,^^!stepy^^!,^^!yb^^!) do (%\n%
+			if ^^!fraction^^! geq 0 set /a "xa+=stepx", "fraction-=dy"%\n%
+			set /a "fraction+=dx"%\n%
+			set "$line=^!$line^![%%y;^!xa^!H "%\n%
+		)%\n%
+	)%\n%
+	set "$line=^!$line^![0m"%\n%
+)) else set args=	
+goto :eof
