670d9b519f36ed884d920cd3d3e8dd1030a7d628
Icarus Lives <fg92sk8r@yahoo.com>
2024-12-28 15:42:05 -0500

    Update update.bat

--- update.bat
+++ update.bat
@@ -9,18 +9,17 @@ exit
 
 :selfUpdate
 	set "currentVersion=1.0.1"
-
 	set "mainPath=https://raw.githubusercontent.com/IcarusLivesHF/YouTube_Resources/refs/heads/main"
 	set "remoteVersionUrl=%mainPath%/version.txt"
+	set "remoteUpdateUrl=%mainPath%/update.bat"
+	
 	set "vFile=%temp%\%~n0version.txt"
 	curl -s -o "%vFile%" "%remoteVersionUrl%"
 	for /f "usebackq delims=" %%i in ("%vFile%") do set "remoteVersion=%%i"
-	
 	if %currentVersion:.=% lss %remoteVersion:.=% (
-		echo New version available. Updating...
-		curl -s -o "%~nx0" "%mainPath%/update.bat"
+		curl -s -o "%~nx0" "%remoteUpdateUrl%"
 		start "" "%~nx0"
-		exit /b
+		exit
 	)
 	for %%i in (currentVersion mainPath remoteVersion remoteVersionUrl vFile) do set "%%~i="
 goto :eof
